3437e1e60c8a24f73d82c3aae07ee708c123751a
Peace Kariuki <kariukipeace3@gmail.com>
2026-09-20 13:06:07 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,76 @@
+print("Hello, world")
+#This is a comment.
+print("Hello, world")
+#This is a comment.
+print("Hello, world")
+5>2
+print("Five is greater than two!")
+x=5
+print(x)
+y=("Hello, world!")
+print(y)
+5>2
+print("yes")
+print("python is fun")
+print("Hello, world!")
+print("Have a good day!")
+print("learning python is fun!")
+print("Hello");print("How are you?");print("Bye bye!")
+print("python is fun!");print("really!")
+print('This will also work')
+print("Hello, world!", end =" ")
+print("I will print on the same line.")
+print(3)
+print(358)
+print(5000)
+print(3+3)
+print(2*5)
+#print("Hello, world!")
+print("Cheers, Mated")
+""" This is a comment 
+written in 
+more than one line 
+"""
+print("Hello, world!")
+x=5
+y="John"
+print(x)
+print(y)
+x=4
+x="Sally"
+print(x)
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+x=5
+y="John"
+print(type(x))
+print(type(y))
+myvar="john"
+my_var="john"
+_my_Var="john"
+myVar="john"
+MYVAR="john"
+myvar2="john"
+print(myvar)
+print(my_var)
+print(_my_Var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+fruits="apple","banana","cherry"
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
